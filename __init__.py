8584283b6cb125309e1bbb106dbf942d62aebaa3
Spencer Alves <impiaaa@gmail.com>
2020-11-06 02:47:24 +0000

    Initial commit

--- __init__.py
+++ __init__.py
@@ -1,15 +1,26 @@
-from mycroft import MycroftSkill, intent_file_handler
+from lango.matcher import match_rules
+from mycroft.skills.common_query_skill import CommonQuerySkill, CQSMatchLevel
+from nlquery.nlquery import NLQueryEngine
+from nlquery.utils import first
 
-
-class Wikidata(MycroftSkill):
+class WikidataSkill(CommonQuerySkill):
     def __init__(self):
-        MycroftSkill.__init__(self)
+        super().__init__()
+        self.engine = NLQueryEngine('localhost', 9000)
+        self.engine.logger = self.log
 
-    @intent_file_handler('wikidata.intent')
-    def handle_wikidata(self, message):
-        self.speak_dialog('wikidata')
-
+    def CQS_match_query_phrase(self, utt):
+        sent = self.engine.preprocess(utt)
+        tree = self.engine.parser.parse(sent)
+        if len(tree) == 0:
+            return
+        ans = first([
+            match_rules(tree, self.engine.find_entity_rules, self.engine.find_entity_query),
+            match_rules(tree, self.engine.subject_prop_rules, self.engine.subject_query),
+        ])
+        if ans and ans.data:
+            return (utt, CQSMatchLevel.GENERAL, ans.to_plain())
 
 def create_skill():
-    return Wikidata()
+    return WikidataSkill()
 
